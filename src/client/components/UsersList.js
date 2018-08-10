165f2390014141992132b9c3270f8b74ad321fb5
Erik <erik.secker@cognitivewave.com>
2018-08-10 14:34:32 -0400

    UsersList - react-redux connected component that requests and displays data

---
+++ src/client/components/UsersList.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { fetchUsers } from '../actions';
+
+class UsersList extends Component {
+	componentDidMount() {
+		this.props.fetchUsers();
+	}
+
+	render() {
+		return (
+			<div>
+				Here's a big list of users:
+				<ul>{ this.renderUsers() }</ul>
+			</div>
+		);
+	}
+
+	renderUsers() {
+		return this.props.users.map( user => {
+			return <li key={user.id}>{ user.name }</li>
+		});
+	}
+}
+
+function mapStateToProps(state) {
+	return {
+		users: state.users,
+	};
+}
+
+UsersList = connect(mapStateToProps, { fetchUsers })(UsersList);
+
+export default UsersList;
