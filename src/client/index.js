9eb3345fae9d5f918a7cb5f00b45ef9109e5bffd
Erik <erik.secker@cognitivewave.com>
2018-08-10 14:31:20 -0400

    babel-polyfill - fixes the "regeneratorRuntime is not defined" error message that gets thrown when using async await axios

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,5 @@
 // start up point for client side application
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
