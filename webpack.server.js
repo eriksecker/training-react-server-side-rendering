4ebc7d6a7c14274953719b9253888c86b7d3840e
Erik <erik.secker@cognitivewave.com>
2018-08-10 10:47:11 -0400

    server optimization: using webpack-node-externals keeps local node_modules packages from being included in the bundle.js increasing start up speed

--- webpack.server.js
+++ webpack.server.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const merge = require('webpack-merge');
+const webpackNodeExternals = require('webpack-node-externals');
 const baseConfig = require('./webpack.base.js');
 
 const config = {
@@ -13,7 +14,9 @@ const config = {
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'build')
-	}
+	},
+
+	externals: [webpackNodeExternals()]
 };
 
 module.exports = merge(baseConfig, config);
